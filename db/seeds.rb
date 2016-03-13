37cd7332028104962f09c456f05d5651f9dd4394
jon.takagi <40095@jisedu.or.id>
2016-03-13 12:05:06 +0700

    logo upload

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,3 @@
 Project.delete_all
 SuccessCondition.delete_all
 Task.delete_all
-Project.create(:name => 'Memes')
-SuccessCondition.create(:description => 'is dank', :completed? => 0, :project_id => Project.where(:name => 'Memes'))
-Task.create!(:description => 'learn HIS NAME', :status => 0, :project_id => Project.where(:name => 'Memes').first.id, :user_id => User.where(:name => 'Jonathan TAKAGI').first.id)
-Task.create!(:description => 'give monkey anxiety', :status => 0, :project_id => Project.where(:name => 'Memes').first.id, :user_id => User.where(:name => 'Jonathan TAKAGI').first.id)
-
-Project.create(:name => 'STAR WARS')
-SuccessCondition.create!(:description => 'blow up death ball', :completed? => 0, :project_id => Project.where(:name => 'STAR WARS'))
-Task.create!(:description => 'pew pew pew', :status => 0, :project_id => Project.where(:name => 'STAR WARS').first.id, :user_id => User.where(:name => 'Jonathan TAKAGI').first.id)
-Task.create!(:description => 'swoosh swoosh swoosh', :status => 0, :project_id => Project.where(:name => 'STAR WARS').first.id, :user_id => User.where(:name => 'Jack Kinghoff').first.id)
